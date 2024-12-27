0bdd628c002ae2aada5473cf3bf9c89c48909e0b
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2024-12-27 08:11:59 -0800

    Change BG color

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,7 @@
 import { Game } from "./game/game";
 
 const urlParams = new URLSearchParams(window.location.search);
-const numFruits = parseInt(urlParams.get('numFruits') || '2');
+const numFruits = parseInt(urlParams.get('players') || '2');
 const game = new Game(numFruits);
 
 game.start();
